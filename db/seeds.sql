b27a3d77335ecf8b1a88f796c4458eaad69d358c
Coomba <coombapace@gmail.com>
2019-05-13 08:15:31 -0400

    database created

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,8 @@
 
 
+INSERT INTO burgers (burger_name) VALUES ('Bulgogi Kimchi Burger');
+INSERT INTO burgers (burger_name) VALUES ('Southwest Style Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken n Waffles Sandwich');
+INSERT INTO burgers (burger_name) VALUES ('Classic Dbl Patty Burger w/ Bacon');
+INSERT INTO burgers (burger_name) VALUES ('Italian Beef n Pepper Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom n Provolone', true);
